a800a96acf3122091bdb532e09e29069e77404f3
JuMiLuGa <lubian.ganete.juliomiguel.ald@gmail.com>
2024-01-09 19:02:55 +0100

    09/01/2024

---
+++ Procesos/Proyectos Java/Procesos/src/Tarea_6/ContadorVocal.java
@@ -0,0 +1,30 @@
+package Tarea_6;
+
+public class ContadorVocal implements Runnable{
+
+    private String vocal;
+    private String input;
+    private Contador contador;
+
+
+    public ContadorVocal(String vocal, String input, Contador contador) {
+        this.vocal = vocal;
+        this.input = input;
+        this.contador = contador;
+    }
+
+    @Override
+    public void run() {
+        int contadorVocal = 0;
+
+        for (int i = 0; i< input.length(); i++) {
+            if (Character.toString(input.charAt(i)).equalsIgnoreCase(vocal)) {
+                contadorVocal++;
+                contador.increment();
+            }
+        }
+
+        System.out.println("Hilo para la vocal " + vocal + ". Total: " + contadorVocal);
+    }
+}
+
